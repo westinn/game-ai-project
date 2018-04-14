b3ddd7b34205616008c1b7227fc0ab2c119d88da
Nicolas Westin <nicolaswestin@gmail.com>
2018-04-14 15:57:57 -0400

    lib issues

--- lib/gym-rle/gym_rle/envs/roms/readme.txt
+++
@@ -1,2 +0,0 @@
-In this directory goes the SNES ROM files (ending in .sfc or .smc).
-File names should be lowercase separated by underscores.
--- lib/gym-rle/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(name='gym_rle',
-      version='0.0.1',
-      install_requires=['gym>=0.2.3',
-                        'rle_python_interface>=1.1.5', 'PyOpenGL']
-)
